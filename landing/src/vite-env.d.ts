0d90b5c08fa6214bf6b3cff221c3dc1695d57fdd
Lissy BARRO <lbarro@Lissys-MacBook-Pro.local>
2025-04-24 20:57:16 +0200

    Landing page

--- landing/src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-declare module '*.svg' {
-  import * as React from 'react'
-  const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>
-  const content: string
-  export { ReactComponent }
-  export default content
-}
